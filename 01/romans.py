19e188c8de5ddcf9db77dd540619a8adcdb635c2
PedroBevilaqua <168040450+bevilaquapg@users.noreply.github.com>
2024-06-09 19:16:18 -0300

    fiz o roman_to_int 👀

--- 01/romans.py
+++ 01/romans.py
@@ -14,6 +14,27 @@ def int_to_roman(num):
 
     return traducao
     
-def roman_to_int(s):
-    # Implemente sua função aqui
-    pass
+def roman_to_int(roman):
+    
+    roman_numerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for numeral in reversed(roman):
+        value = roman_numerals[numeral]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+
+    return total
